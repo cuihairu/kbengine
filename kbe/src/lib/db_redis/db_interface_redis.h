4cc2a6490840340a8eb53e678e2f1db725ecf3f7
cuihairu <chuihairu@gmail.com>
2026-04-01 05:55:39 +0800

    fix: use vcpkg hiredis headers on windows

--- kbe/src/lib/db_redis/db_interface_redis.h
+++ kbe/src/lib/db_redis/db_interface_redis.h
@@ -10,14 +10,7 @@
 #include "helper/debug_helper.h"
 #include "db_interface/db_interface.h"
 
-#include "hiredis.h"
-#if KBE_PLATFORM == PLATFORM_WIN32
-#ifdef _DEBUG
-#pragma comment (lib, "hiredis_d.lib")
-#else
-#pragma comment (lib, "hiredis.lib")
-#endif
-#endif
+#include <hiredis/hiredis.h>
 
 namespace KBEngine { 
 
